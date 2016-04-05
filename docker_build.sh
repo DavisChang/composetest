8112249e0be4c4fb2f31b7a49a8fd29a0d32bea1
davis_chang <davis chang>
2016-04-05 23:54:33 +0800

    back

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 TIMESTAMP=`date +"%Y%m%d%H%M%S"`
 DATE=$(date +%d-%m-%Y" "%H:%M:%S)
@@ -8,10 +8,15 @@ DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST}
 
 TAG="${BUILD_NUMBER}-${GIT_COMMIT}"
 
-env 
-
 docker build \
   -t "$DOCKER_REPO_NAME:$TAG" \
+  --build-arg GIT_URL="$GIT_URL" \
+  --build-arg GIT_BRANCH="$GIT_BRANCH" \
+  --build-arg GIT_COMMIT="$GIT_COMMIT" \
+  --build-arg BUILD_NUMBER="$BUILD_NUMBER" \
+  --build-arg BUILD_URL="$BUILD_URL" \
+  --build-arg BUILD_USER="$BUILD_USER" \
+  --build-arg BUILD_TIME="$DATE" \
   .
 
 docker tag -f "$DOCKER_REPO_NAME:$TAG" "$DOCKER_REGISTRY_HOST/$DOCKER_REPO_NAME:$TAG"
